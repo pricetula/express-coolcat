700b4eebd450c0690592aa3749e0a78ed38e0817
price tula <pricetulak@gmail.com>
2017-12-19 17:47:34 +0300

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable function-paren-newline */
 const winston = require('winston');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -14,16 +15,38 @@ app.use(
   )
 );
 
+app.use(
+  express.static(
+    path.join(
+      __dirname,
+      'public'
+    )
+  )
+);
+
 app.get(
   '/',
   function (
     req,
     res
   ) {
-    res.status(
-      200
-    ).send(
-      'Cool Cat'
+    res
+      .sendFile(
+        path.join(__dirname + '/public/index.html')
+      );
+  }
+);
+//d
+app.use(
+  function (
+    req,
+    res,
+    next
+  ) {
+    res
+      .status(404)
+      .sendFile(
+        path.join(__dirname + '/public/404.html')
       );
   }
 );
