c5aa5214b2b356558cb27f2dabbf6424d60b36e2
price tula <pricetulak@gmail.com>
2017-12-20 15:20:29 +0300

    html email

--- api/user/utils/validate-mailer.js
+++ api/user/utils/validate-mailer.js
@@ -5,11 +5,18 @@ module.exports = (
   address,
   subject,
   token
-) => mailTranspoter.sendMail(
-  {
-    to: address,
-    subject,
-    text: `Click link to activate account ${appConfig.url}/user/verify/${token}`
-  },
-  (err) => console.log(err)
-);
+) => {
+  mailTranspoter.sendMail(
+    {
+      to: address,
+      subject,
+      html: `
+      <div>
+        <p style="font-size:1.2em;color:#444;font-weight:bold;">Click button below activate account</p> 
+        <br/>
+        <a style="background-color: #CE0985;border: none;border-radius: 1em;color: white;padding: 1em;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;font-weight:bold;" href="${appConfig.url}/user/verify/${token}">Activate</a>
+      </div>`
+    },
+    (err) => console.log(err)
+  );
+}
