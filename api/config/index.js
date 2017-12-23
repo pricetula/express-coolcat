bf961a84e960c53052d212d1de0164626f1a2edc
price tula <pricetulak@gmail.com>
2017-12-23 16:31:06 +0300

    using local mongo for dev

--- api/config/index.js
+++ api/config/index.js
@@ -1,17 +1,18 @@
-let port = process.env.PORT;
+let port = process.env.PORT || 3030;
 let url = 'https://express-coolcat.herokuapp.com';
 let mongoDBconnectionUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://coolcat:funguamlab1@ds129146.mlab.com:29146/coolcat';
 
 if (process.env.NODE_ENV === 'development') {
-  port = 3030;
-  mongoDBconnectionUri = 'mongodb://127.0.0.1:27017'; // 'mongodb://coolcattest:test123@ds137826.mlab.com:37826/coolcattest';
+  // 'mongodb://coolcattest:test123@ds137826.mlab.com:37826/coolcattest';
+  mongoDBconnectionUri = 'mongodb://127.0.0.1:27017/coolcat';
 }
 
 if (process.env.NODE_ENV === 'test') {
   port = 3031;
-  mongoDBconnectionUri = 'mongodb://127.0.0.1:27017'; // 'mongodb://coolcattest:test123@ds137826.mlab.com:37826/coolcattest';
+  // 'mongodb://coolcattest:test123@ds137826.mlab.com:37826/coolcattest';
+  mongoDBconnectionUri = 'mongodb://127.0.0.1:27017';
 }
 
 if (process.env.NODE_ENV !== 'production') {
